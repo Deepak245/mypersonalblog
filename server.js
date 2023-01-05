90e92de1da101e6a34c3d89a8e11204a576c2e11
Amrith <amrith.dwadasi@gmail.com>
2023-01-05 22:17:10 +0530

    Modified Path and installCommand

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ if(process.env.ENV === "PROD"){
 // un comment it before going to prod.
 app.get("/*", (req, res) => {
   // res.sendFile(path.resolve(__dirname,"../frontend/build","index.html"))
-  res.sendFile(path.resolve(__dirname,"./frontend/build","index.html"))
+  res.sendFile(path.resolve(__dirname,"./frontend/build","/index.html"))
  });
 }
 
